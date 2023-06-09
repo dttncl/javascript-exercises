eaa4e76b7c2d0e70da87764090a889075ca31144
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-09 11:03:05 -0400

    feat: add 03_reverseString js solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let arr = word.split("");
+    let rev = [];
 
+    for (let i = arr.length; i >= 0 ; i--) {
+        rev.push(arr[i]);
+    }
+
+    return rev.join("");
 };
 
 // Do not edit below this line
