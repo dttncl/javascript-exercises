db47355c4637a5a09366de6fcbb46e8c21698ab9
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-09 12:40:44 -0400

    add solution to 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum,num) => sum+=num,0);	
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args.reduce((product,num) => product*=num,1);	
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+	for (let i = num; i >= 1; i--) {
+    res *= i;
+  }
+
+  return res;
 };
 
 // Do not edit below this line
