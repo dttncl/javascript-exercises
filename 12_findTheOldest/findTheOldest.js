2ba8bea7f5b19052cdfed2456c89f67e72faeb44
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-16 13:11:54 -0400

    add solution to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let maxAge = -1;
+    let oldestPerson;
+    arr.forEach(obj => {
+        if (obj.hasOwnProperty('yearOfDeath')) {
+            let age = obj.yearOfDeath - obj.yearOfBirth;
+            if (age > maxAge) {
+                maxAge = age;
+                oldestPerson = obj;
+            }
+        } else {
+            const date = new Date();
+            let age = date.getFullYear() - obj.yearOfBirth;
+            if (age > maxAge) {
+                maxAge = age;
+                oldestPerson = obj;
+            }
+        }
+    });
 
+    return oldestPerson;
 };
 
 // Do not edit below this line
