b83f4e4d7c52a04714bb50c45e04e4775a88613e
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-16 12:22:38 -0400

    add solution to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num > 0) {
+        let phi = (1 + 5**(1/2))/2;
+        let numerator = phi**num - (1-phi)**num;
+        let denominator = 5**(1/2);
+    
+        return Math.floor(numerator /denominator);
+    }
+
+    return 'OOPS';
 
 };
 
